a5ed92fa7ba0692a9097e542d54de9beeb530807
Badri Srinivasan <badri.it@gmail.com>
2015-02-10 19:37:06 +0000

    removed gtm

--- ui.R
+++ ui.R
@@ -44,7 +44,6 @@ dashboardPage(skin = "black",
       tabItem(tabName = "help",
               tags$a(href="mailto:badrinarayanan.srinivasan@iprospect.com", 
                                        "Feedback / Feature request"))
-    ),
-    tags$body(includeHTML("gtmtag.html"))
+    )
   )
 )
