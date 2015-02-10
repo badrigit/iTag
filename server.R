17898fa54dd0b0d4e34b946d5c99a6314cded24c
Badri Srinivasan <badri.it@gmail.com>
2015-02-10 18:42:33 +0000

    updated other category for channel format

--- server.R
+++ server.R
@@ -2,6 +2,86 @@ library(shiny)
 
 shinyServer(function(input, output) {
   
+  output$channelFormat <- renderUI({
+    if(input$channel == 'ppc')
+      list(
+        selectInput(inputId = "po", "Paid / Organic", paidOnly),
+        selectInput(inputId = "format", "Channel Format", ppcFormatType),
+        uiOutput(outputId = "otherChannelFormat"),
+        radioButtons(inputId = "bbb", "Build / Borrow / Buy", c("Build" = "ui"))
+    ) else if(input$channel == 'outbrain'){
+      list(
+        selectInput(inputId = "po", "Paid / Organic",paidOnly),
+        selectInput(inputId = "format", "Channel Format", outbrainFormatType),
+        uiOutput(outputId = "otherChannelFormat"),
+        radioButtons(inputId = "bbb", "Build / Borrow / Buy", 
+                     c("Build" = "ui", "Borrow" = "or","Buy" = "uy"))
+      )
+    } else if(input$channel == 'taboola'){
+      list(
+        selectInput(inputId = "po", "Paid / Organic",paidOnly),
+        selectInput(inputId = "format", "Channel Format", taboolaFormatType),
+        uiOutput(outputId = "otherChannelFormat"),
+        radioButtons(inputId = "bbb", "Build / Borrow / Buy", 
+                     c("Build" = "ui", "Borrow" = "or","Buy" = "uy"))
+      )
+    } else if(input$channel == 'trueview'){
+      list(
+        selectInput(inputId = "po", "Paid / Organic",paidOnly),
+        selectInput(inputId = "format", "Channel Format", trueviewFormatType),
+        uiOutput(outputId = "otherChannelFormat"),
+        radioButtons(inputId = "bbb", "Build / Borrow / Buy", 
+                     c("Build" = "ui", "Borrow" = "or","Buy" = "uy"))
+      )
+    } else if(input$channel == 'display'){
+      list(
+        selectInput(inputId = "po", "Paid / Organic",paidOnly),
+        selectInput(inputId = "format", "Channel Format", displayFormatType),
+        uiOutput(outputId = "otherChannelFormat"),
+        radioButtons(inputId = "bbb", "Build / Borrow / Buy", 
+                     c("Build" = "ui", "Borrow" = "or","Buy" = "uy"))
+      )
+    } else if(input$channel == 'facebook'){
+      list(
+        selectInput(inputId = "po", "Paid / Organic",paidOrganic),
+        selectInput(inputId = "format", "Channel Format", facebookFormatType),
+        uiOutput(outputId = "otherChannelFormat"),
+        radioButtons(inputId = "bbb", "Build / Borrow / Buy", 
+                     c("Build" = "ui", "Borrow" = "or","Buy" = "uy"))
+      )
+    } else if(input$channel == 'youtube'){
+      list(
+        selectInput(inputId = "po", "Paid / Organic",paidOrganic),
+        selectInput(inputId = "format", "Channel Format", youtubeFormatType),
+        uiOutput(outputId = "otherChannelFormat"),
+        radioButtons(inputId = "bbb", "Build / Borrow / Buy", 
+                     c("Build" = "ui", "Borrow" = "or","Buy" = "uy"))
+      )
+    } else if(input$channel == 'twitter'){
+      list(
+        selectInput(inputId = "po", "Paid / Organic",paidOrganic),
+        selectInput(inputId = "format", "Channel Format", twitterFormatType),
+        uiOutput(outputId = "otherChannelFormat"),
+        radioButtons(inputId = "bbb", "Build / Borrow / Buy", 
+                     c("Build" = "ui", "Borrow" = "or","Buy" = "uy"))
+      )
+    } else if(input$channel == 'linkedin'){
+      list(
+        selectInput(inputId = "po", "Paid / Organic",paidOrganic),
+        selectInput(inputId = "format", "Channel Format", linkedinFormatType),
+        uiOutput(outputId = "otherChannelFormat"),
+        radioButtons(inputId = "bbb", "Build / Borrow / Buy", 
+                     c("Build" = "ui", "Borrow" = "or","Buy" = "uy"))
+      )
+    }
+  })
+  
+  output$otherChannelFormat <- renderUI({
+    if(input$format == "other"){
+      textInput("otherChannel", "(Other Channel Format)")
+    }
+  })
+  
   parameters <- reactive({
       if(input$submit == 0)
         return("Fill all the details")
@@ -14,20 +94,33 @@ shinyServer(function(input, output) {
             return("Missing Campaign Name")
           } else if((input$campaign == "other") && (input$otherCampaign != "")){
             if(input$cname != ""){
-              tag <- paste(input$pr, input$otherCampaign, input$product, 
-                           input$channel, input$po, channelFormat(),
-                           tolower(input$cname), bbb(), sep = "_")
-              code <- paste(url, "?cid=cd_", tag, sep = "")
+              if(input$format == "other"){
+                tag <- paste(input$pr, input$otherCampaign, input$product, 
+                             input$channel, input$po, input$otherChannel,
+                             tolower(input$cname), input$bbb, sep = "_")
+                code <- paste(url, "?cid=cd_", tag, sep = "")
+              } else {
+                tag <- paste(input$pr, input$otherCampaign, input$product, 
+                             input$channel, input$po, input$format,
+                             tolower(input$cname), input$bbb, sep = "_")
+                code <- paste(url, "?cid=cd_", tag, sep = "")
+              }
             } else if(input$cname == ""){
               return("Missing Creative Name")
             }
           } else {
             if(input$cname != ""){
-              tag <- paste(input$pr, input$campaign, input$product, input$channel, 
-                           input$po, channelFormat(), tolower(input$cname), bbb(), sep = "_")
-              code <- paste(url, "?cid=cd_", tag, sep = "")
-            } else if(input$cname == ""){
-              return("Missing Creative Name")
+              if(input$format == "other"){
+                tag <- paste(input$pr, input$campaign, input$product, 
+                             input$channel, input$po, input$otherChannel,
+                             tolower(input$cname), input$bbb, sep = "_")
+                code <- paste(url, "?cid=cd_", tag, sep = "")
+              } else {
+                tag <- paste(input$pr, input$campaign, input$product, 
+                             input$channel, input$po, input$format,
+                             tolower(input$cname), input$bbb, sep = "_")
+                code <- paste(url, "?cid=cd_", tag, sep = "")
+              }
             }
           }
         } else {
--- ui.R
+++ ui.R
@@ -10,41 +10,7 @@ dashboardPage(
       box(width = 2, height = 4,
           selectInput(inputId = "pr", "Planned or Responsive", plre),
           selectInput(inputId = "channel", "Channel",channelInput),
-          conditionalPanel("input.channel == 'ppc'", 
-                           selectInput(inputId = "po", "Paid / Organic",paidOnly),
-                           selectInput(inputId = "format", "Channel Format", ppcFormatType)),
-          
-          conditionalPanel("input.channel == 'outbrain'", 
-                           selectInput(inputId = "po", "Paid / Organic",paidOnly),
-                           selectInput(inputId = "format", "Channel Format", outbrainFormatType)),
-          
-          conditionalPanel("input.channel == 'taboola'", 
-                           selectInput(inputId = "po", "Paid / Organic",paidOnly),
-                           selectInput(inputId = "format", "Channel Format",taboolaFormatType)),
-          
-          conditionalPanel("input.channel == 'trueview'", 
-                           selectInput(inputId = "po", "Paid / Organic",paidOnly),
-                           selectInput(inputId = "format", "Channel Format",trueviewFormatType)),
-          
-          conditionalPanel("input.channel == 'display'", 
-                           selectInput(inputId = "po", "Paid / Organic",paidOnly),
-                           selectInput(inputId = "format", "Channel Format",displayFormatType)),
-          
-          conditionalPanel("input.channel == 'facebook'", 
-                           selectInput(inputId = "po", "Paid / Organic",paidOrganic),
-                           selectInput(inputId = "format", "Channel Format",facebookFormatType)),
-          
-          conditionalPanel("input.channel == 'youtube'", 
-                           selectInput(inputId = "po", "Paid / Organic",paidOrganic),
-                           selectInput(inputId = "format", "Channel Format", youtubeFormatType)),
-          
-          conditionalPanel("input.channel == 'twitter'", 
-                           selectInput(inputId = "po", "Paid / Organic",paidOrganic),
-                           selectInput(inputId = "format", "Channel Format", twitterFormatType)),
-          
-          conditionalPanel("input.channel == 'linkedin'", 
-                           selectInput(inputId = "po", "Paid / Organic",paidOrganic),
-                           selectInput(inputId = "format", "Channel Format", linkedinFormatType))
+          uiOutput(outputId = "channelFormat")
       ),
       
       box(width = 2, height = 4,
@@ -57,14 +23,6 @@ dashboardPage(
           textInput(inputId = "url", label = "URL"),
           
           textInput(inputId = "cname", "Creative / Ad Name"),
-          
-          conditionalPanel("input.channel == 'ppc'", 
-                           selectInput(inputId = "bbb", "Build / Borrow / Buy",
-                                        c("Build" = "ui"))),
-          
-          conditionalPanel("input.channel != 'ppc'", 
-                           selectInput(inputId = "bbb", "Build / Borrow / Buy",
-                                        c("Build" = "ui", "Borrow" = "or","Buy" = "uy"))),
           
           actionButton(inputId = "submit", label = "Submit")
       ),
